f1f4e64ebf58ff2dbd3751b6b089e06b532cbd3f
darkfinger <892810dkcbak@G>
2018-05-26 21:09:36 -0400

    main

---
+++ src/main/java/com/dkkcorp/springsecurity/demo/App.java
@@ -0,0 +1,9 @@
+package com.dkkcorp.springsecurity.demo;
+
+
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/dkkcorp/springsecurity/demo/config/SecurityWebAppInitializer.java
@@ -0,0 +1,6 @@
+package com.dkkcorp.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
+}
