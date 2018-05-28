9ead5f415b6081582ba11de411c97fab4f4b578b
darkfinger <892810dkcbak@G>
2018-05-28 16:21:10 -0400

    html added for each role

--- src/main/java/com/dkkcorp/sprinnboot/springsecuritydemo/controller/HomeController.java
+++ src/main/java/com/dkkcorp/sprinnboot/springsecuritydemo/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.dkkcorp.sprinnboot.springsecuritydemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,4 +11,18 @@ public class HomeController {
     @RequestMapping({"/","","index"})
     public String index(Model model){
         return "view/index";
-    }}
+    }
+    @GetMapping("/manager/")
+    public String managers(){
+        return "manager-space";
+    }
+    @GetMapping("/admin/")
+    public String admin(){
+        return "admin-space";
+    }
+    @GetMapping("/common/")
+    public String common(){
+        return "common-space";
+    }
+}
+
