f32102ddd26331853a2911c3a177ba02c1a789f3
darkfinger <892810dkcbak@G>
2018-05-29 13:02:33 -0400

    jdbc Connection

--- src/main/java/com/dkkcorp/sprinnboot/springsecuritydemo/config/DemoSecurityConfig.java
+++ src/main/java/com/dkkcorp/sprinnboot/springsecuritydemo/config/DemoSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -16,6 +17,9 @@ import javax.sql.DataSource;
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private DataSource dataSource;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public DemoSecurityConfig(DataSource dataSource){
         this.dataSource=dataSource;
@@ -30,8 +34,8 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
                 jdbcAuthentication()
                 .usersByUsernameQuery("select username, password, enable from user where username=?")
                 .authoritiesByUsernameQuery("select u.username, r.role from user u inner join user_role ur on(u.id=ur.user_id) inner join role r on(ur.role_id=r.id) where u.username=?\n")
-                .dataSource(dataSource);
-                //.passwordEncoder(bCryptPasswordEncoder);
+                .dataSource(dataSource)
+                .passwordEncoder(bCryptPasswordEncoder);
 
         //in memory login
 //        User.UserBuilder userBuilder=User.withDefaultPasswordEncoder();
@@ -44,7 +48,7 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").hasRole("EMP0")
+                .antMatchers("/").hasRole("EMPO")
                 .antMatchers("/manager/**").hasRole("MAN")
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/common/**").hasAnyRole("MAN","ADMIN")
--- src/main/java/com/dkkcorp/sprinnboot/springsecuritydemo/service/UserServiceImpl.java
+++ src/main/java/com/dkkcorp/sprinnboot/springsecuritydemo/service/UserServiceImpl.java
@@ -3,12 +3,15 @@ package com.dkkcorp.sprinnboot.springsecuritydemo.service;
 import com.dkkcorp.sprinnboot.springsecuritydemo.model.User;
 import com.dkkcorp.sprinnboot.springsecuritydemo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     @Autowired
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -19,6 +22,9 @@ public class UserServiceImpl implements UserService {
         if(user==null){
             throw new RuntimeException("null value for user");
         }
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setEnable(true);
+
         User userSaved=userRepository.save(user);
         System.out.println("============>>>>>>>>User saved : "+userSaved.getUsername());
         return userSaved;
